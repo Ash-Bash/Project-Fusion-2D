d7c69f5562a47f00574bc63583e3479fb92ed6d8
Ash-Bash <xpsx.fan@googlemail.com>
2014-04-21 21:33:03 +0100

    Added a BikeController.cs Script

---
+++ Assets/Game_Dir/Scripts/BikeController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeController : MonoBehaviour {
+
+	public GameObject BackBikeWheel;
+
+	public float BackWheelSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void FixedUpdate(){
+
+		if(Input.GetKeyDown(KeyCode.A)){
+
+			BackBikeWheel.rigidbody2D.AddTorque(BackWheelSpeed);
+
+		}
+
+	}
+}
